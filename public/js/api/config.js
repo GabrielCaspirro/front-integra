0386cd632f74350007f9a916fce4823e99d34b0f
GabrielCaspirro <gabriel.gcd08@gmail.com>
2025-10-07 00:29:09 -0300

    server

--- public/js/api/config.js
+++ public/js/api/config.js
@@ -1,4 +1,4 @@
-//export const BASE_URL = "https://api-integra.vercel.app/integra-api";
-//export const BASE_URL_IMG = "https://api-integra.vercel.app/";
-export const BASE_URL = "http://localhost:3001/integra-api";
-export const BASE_URL_IMG = "http://localhost:3001/";
+export const BASE_URL = "https://api-integra.vercel.app/integra-api";
+export const BASE_URL_IMG = "https://api-integra.vercel.app/";
+//export const BASE_URL = "http://localhost:3001/integra-api";
+//export const BASE_URL_IMG = "http://localhost:3001/";
